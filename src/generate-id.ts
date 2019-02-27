9a2b703c20f1737a3c80490ada791eac8c284f86
Steven <steven@ceriously.com>
2019-02-27 12:17:32 -0500

    Change generateId() to be 64 bytes wide

--- src/generate-id.ts
+++ src/generate-id.ts
@@ -1,5 +1,19 @@
-export const generateId = () =>
-  Math.random()
-    .toString(16)
-    .slice(2)
-    .toUpperCase();
+export const generateId = (bytes = 64) =>
+  range(bytes)
+    .map(getRandomByte)
+    .map(toHex)
+    .join('');
+
+const range = (length: number) => Array.from({ length });
+
+const getRandomBit = () => Math.round(Math.random());
+
+const concatenateBits = (accumulator: number, bit: number, i: number) =>
+  accumulator + (bit << i);
+
+const getRandomByte = () =>
+  range(8)
+    .map(getRandomBit)
+    .reduce(concatenateBits);
+
+const toHex = (n: number) => n.toString(16).toUpperCase().padStart(2, '0');
--- test/generate-id.test.ts
+++ test/generate-id.test.ts
@@ -1,10 +1,9 @@
 import test from 'tape';
 import { generateId } from '../src/generate-id';
 
-test('generate-id test', t => {
-  t.plan(3);
-  const id = generateId();
-  t.true(typeof id === 'string');
-  t.true(id.length > 5);
-  t.true(id.length < 64);
+test('generate-id test length', t => {
+  t.plan(1);
+  const bytes = 1024;
+  const id = generateId(bytes);
+  t.equal(id.length, bytes * 2, 'each byte is represented as 2 chars');
 });
